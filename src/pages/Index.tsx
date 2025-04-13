46b134a013a22121e370ffb46f50902795ae4c28
Maharshi Desai <133489200+Maharshi-24@users.noreply.github.com>
2025-04-13 18:33:57 +0530

    Removed the scroll bar.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,7 +8,7 @@ import Footer from "@/components/Footer";
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-gray-100 hide-scrollbar">
       <Navigation />
       <main>
         <HeroSection />
